bad88b5f88caed2d7a05b6e61c18b7f6b647e628
DANIEL WILLIAMS MENSAH <83681534+Williams9007@users.noreply.github.com>
2025-11-28 06:10:50 -0600

    Delete backend directory

--- backend/Controllers/roleController.js
+++
@@ -1,5 +0,0 @@
-export const assignRole = (req, res) => {
-  const { userId, role } = req.body;
-  // TODO: update user role in DB
-  res.json({ success: true, message: `Role ${role} assigned to user ${userId}` });
-};
--- backend/cedis/formatCurrency.js
+++
@@ -1,7 +0,0 @@
-// utils/formatCurrency.js
-export const formatCurrency = (amount) => {
-  return new Intl.NumberFormat("en-GH", {
-    style: "currency",
-    currency: "GHS",
-  }).format(amount);
-};
